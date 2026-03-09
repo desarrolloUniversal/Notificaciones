1f4f90010c0fbe6dbb28d79ab863ffb99325b609
desarrollouniversal1 <desarrollouniversal3@gmail.com>
2026-03-09 13:32:40 -0600

    fix-deploy /

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #Comandos para automatizar el despliegue en el servidor remoto
-rsync -avz --delete ./dist pruebasdesarrollo@172.16.250.230:/var/www/desarrollo.eluniversal.com.mx/notificaciones
+rsync -avz --delete ./dist/ pruebasdesarrollo@172.16.250.230:/var/www/desarrollo.eluniversal.com.mx/notificaciones
 echo "¡Despliegue completado!"
