7183442908925e7ad3888f82e0c9c05040158d5c
desarrollouniversal1 <desarrollouniversal3@gmail.com>
2026-04-21 16:41:45 -0600

    fix-server

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-#Comandos para automatizar el despliegue en el servidor remoto
-rsync -avz --delete ./dist pruebasdesarrollo@172.16.250.230:/var/www/desarrollo.eluniversal.com.mx/notificaciones
+# El / después de dist es la clave para que copie el CONTENIDO y no la CARPETA
+rsync -avz --delete ./dist/ pruebasdesarrollo@172.16.250.230:/var/www/desarrollo.eluniversal.com.mx/notificaciones/dist/
+
 echo "¡Despliegue completado!"
